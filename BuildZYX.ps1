076d4de6cca8111b56873a6629fd1eff2db39189
StarwalkerZYX <zhouyongxin@263.net>
2018-11-19 23:28:51 +0800

    增加BuildZYX.ps1。 直接执行BuildZYX.ps1来完成编译。 build.py中增加编译过程中发现缺少的include path。 卡在错误：link.exe提示无法解析THLong***等定义。

--- BuildZYX.ps1
+++ BuildZYX.ps1
@@ -1,10 +1,7 @@
-#Build NMS
-
- 使用PowerShell
-```
 $PathVariable = $env:path
 $PathVariable = $PathVariable+";C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64"
 $PathVariable = $PathVariable+";C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.0\bin\"
+$PathVariable = $PathVariable+";C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.15.26726\bin\Hostx64\x64"
 
 $Env:path= $PathVariable
 
@@ -14,14 +11,6 @@ nvcc -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_35
 
 cd ../../
 
-$PythonExe = 'C:\Users\zhou.yongxin\AppData\Local\conda\conda\envs\PyTorch\python.exe'
+#$PythonExe = 'C:\Users\zhou.yongxin\AppData\Local\conda\conda\envs\PyTorch\python.exe'
+$PythonExe = 'e:\Anaconda3\envs\pytorch\python.exe'
 &$PythonExe build.py
-
-cd ../
-```
-
- cd roialign/roi_align/src/cuda/
- nvcc -c -o crop_and_resize_kernel.cu.o crop_and_resize_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_35
- cd ../../
- python build.py
- cd ../../
--- third_party/nms/build.py
+++ third_party/nms/build.py
@@ -21,6 +21,14 @@ this_file = os.path.dirname(os.path.realpath(__file__))
 print(this_file)
 extra_objects = [os.path.join(this_file, fname) for fname in extra_objects]
 
+extra_include_dirs = [
+    '"C:/Program Files (x86)/Windows Kits/10/Include/10.0.10240.0/ucrt"',
+    '"C:/Program Files (x86)/Windows Kits/10/Include/10.0.17134.0/shared"',
+    '"C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/include"',
+    r'"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.0\include"'
+    #'"C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.15.26726/include"',
+]
+
 ffi = create_extension(
     '_ext.nms',
     headers=headers,
@@ -28,7 +36,8 @@ ffi = create_extension(
     define_macros=defines,
     relative_to=__file__,
     with_cuda=with_cuda,
-    extra_objects=extra_objects
+    extra_objects=extra_objects,
+    include_dirs=extra_include_dirs,
 )
 
 if __name__ == '__main__':
